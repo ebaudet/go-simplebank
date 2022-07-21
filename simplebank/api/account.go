cc23f79e487d49ba09233396764a17557a2bf161
Emilien Baudet <emilien.baudet@gmail.com>
2022-07-21 15:03:57 +0200

    clean useless logging for account

--- simplebank/api/account.go
+++ simplebank/api/account.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"database/sql"
-	"log"
 	"net/http"
 
 	db "github.com/ebaudet/simplebank/db/sqlc"
@@ -30,7 +29,6 @@ func (server *Server) createAccount(ctx *gin.Context) {
 	account, err := server.store.CreateAccount(ctx, account_params)
 	if err != nil {
 		if pqErr, ok := err.(*pq.Error); ok {
-			log.Println("> ", pqErr.Code.Name())
 			switch pqErr.Code.Name() {
 			case "unique_violation", "foreign_key_violation":
 				ctx.JSON(http.StatusForbidden, errorResponse(err))
